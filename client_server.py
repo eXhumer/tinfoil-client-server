279ad27081c49040cb0f7e3dfc3d15538782a6e8
eXhumer <eXhumer@member.fsf.org>
2021-03-02 06:15:56 +0000

    Serve generic success to valid tinfoil clients

--- client_server.py
+++ client_server.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+from app.utils import valid_tinfoil_client
+import json
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/tinfoil_client_db"
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    if valid_tinfoil_client(mongo):
+        return json.dumps({"success": "Loaded index successfully!"})
+
+    return "<h1>Bad Request!</h1>", 400
